e3fc192ffc4a381084b982100f83badd3ad10526
divyakumari11 <48504113+divyakumari11@users.noreply.github.com>
2021-09-12 19:20:24 +0530

    background-img& spread operator addition

--- RestrauWebApp/restrauapp/src/component/basics/Resturant.js
+++ RestrauWebApp/restrauapp/src/component/basics/Resturant.js
@@ -3,12 +3,22 @@ import "./style.css";
 import Menu from "./menuApi.js";
 import MenuCard from "./MenuCard";
 import Navbar from "./Navbar";
+
+
+// this (...) is a spread operator used for converting the object data received from the API into array and then array fetching.
+const uniqueList = [
+    ...new Set(
+        Menu.map((curElem) => {
+            return curElem.category;
+        })
+    ),
+];
 const Resturant = () => {
     const [menuData, setMenuData] = useState(Menu);
 
     const filterItem = (category) => {
         const updatedList = Menu.filter((curElem) => {
-            return curElem.category == category;
+            return curElem.category === category;
         });
         setMenuData(updatedList);
     }
@@ -21,7 +31,7 @@ const Resturant = () => {
                 <button className="btn-group__item" onClick={() => filterItem("Lunch")}>Lunch</button>
                 <button className="btn-group__item" onClick={() => filterItem("Dinner")}>Dinner</button>
                 <button className="btn-group__item" onClick={() => filterItem("Snacks")}>Snacks</button>
-                {/* <button className="btn-group__item">Dessert</button> */}
+                <button className="btn-group__item" onClick={() => filterItem("Dessert")}>Dessert</button>
                 <button className="btn-group__item" onClick={() => setMenuData(Menu)}>All</button>
             </div>
         </nav>
